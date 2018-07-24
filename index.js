b42e068f13e5cfa76407053216211552b32a3b5f
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-24 20:53:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function lowerCaseDrivers(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map( function(driver) { return Object.assign({}, driver) driver.toLowerCase()} )
+  return drivers.map( function(driver) { return  driver.toLowerCase()} )
 }
 
  return Object.assign({}, account, { accessLevel: 'admin' })
