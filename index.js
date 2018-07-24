a6ecfa202b78a72849c59003c5cb0cdb4fbda2e6
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-24 22:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  drivers.map(drivers, function(driver) { return driver.toLowerCase()} );
+  drivers.map(function(driver) { return driver.toLowerCase()} );
 
 }
 
