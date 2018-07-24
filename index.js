7a5ab9495a2bae8d89f271ea6951d5680d2ff7af
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-24 20:54:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  return drivers.map(drivers, function(driver) { return driver.toLowerCase()} )
+  const lowerDrivers = drivers.map(drivers, function(driver) { return driver.toLowerCase()} )
 }
 
 function attributesToPhrase(drivers) {
